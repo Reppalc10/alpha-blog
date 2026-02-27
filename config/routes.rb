1eb7c05958318cf8573be1cf14d11e924b712ac8
Trevor Clapper <trevorclapper@gmail.com>
2026-02-27 09:41:47 -0600

    created articles list page + table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home" # send root route to a controller
   get "about", to: "pages#about" # creates get request to route to about page
-  resources :articles, only: [ :show ]
+  resources :articles, only: [ :show, :index ]
 end
