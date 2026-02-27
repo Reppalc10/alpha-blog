69dfee552a37602d022b728fec6a193bceb69000
Trevor Clapper <trevorclapper@gmail.com>
2026-02-27 11:18:04 -0600

    edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,32 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Successfully created!" # creates message at the top of page after success (called in application.html.erb)
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated successfully."
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
 end
