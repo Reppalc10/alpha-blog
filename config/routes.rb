c6e3b8a78d6c96a6bbbda1dc87dffe8e649785e2
Trevor Clapper <trevorclapper@gmail.com>
2026-03-02 13:52:27 -0600

    working on adding signup for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "pages#home" # send root route to a controller
   get "about", to: "pages#about" # creates get request to route to about page
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [ :new ]
 end
